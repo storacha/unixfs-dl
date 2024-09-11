8ef9e1bc4d2a7374c1957e189ff9b8fb307250b1
Alan Shaw <alan.shaw@protocol.ai>
2024-09-11 11:37:55 +0200

    chore: add github actions

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export declare function fetch (url: string|URL): Response
--- test/test.js
+++ test/test.js
@@ -1,20 +1,21 @@
-import { fetch } from './index.js'
+import { fetch } from '../index.js'
 
 export const test = {
   'should fetch a big file': async (/** @type {import('entail').assert} */ assert) => {
     const root = 'bafybeigugdohnjpclc3ezehkwg4d3kkw243rdgext5czmww26hfpynlorq'
     const path = '/HRHSweetJeremy%202024.mov'
-    const res = await fetch(`https://w3s.link/ipfs/${root}${path}`)
-    assert.equal(res.ok, true)
-
+    
     let total = 0
     try {
+      const res = await fetch(`https://w3s.link/ipfs/${root}${path}`)
+      assert.equal(res.ok, true)
       for await (const chunk of res.body) {
         total += chunk.length
       }
       console.log(`received ${total} bytes`)
     } catch (err) {
       console.error(err)
+      assert.fail(err.message)
     }
   }
 }
